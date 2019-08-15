d466f4c67232b7570773f6688d306756de498fbb
Chris Richards <ripter001@gmail.com>
2019-08-14 22:04:57 -0700

    Adding a group of items on start

--- 2019/src/entities/ShapedItem.js
+++ 2019/src/entities/ShapedItem.js
@@ -1,7 +1,7 @@
 import { updateElement } from '../utils/updateElement.js';
 
 export class ShapedItem {
-  constructor(x, y, z, icon = 'HEART') {
+  constructor(icon = 'HEART') {
     this.scene = document.querySelector('a-scene');
     this.el = document.createElement('a-entity');
 
@@ -13,9 +13,9 @@ export class ShapedItem {
     this.state = {
       'cursor-listener': 'cursor-listener',
       position: {
-        x: x || 0,
-        y: y || 0,
-        z: z || 0,
+        x: 0,
+        y: 0,
+        z: 0,
       },
       rotation: {
         x: 0,
--- 2019/src/systems/game.js
+++ 2019/src/systems/game.js
@@ -12,17 +12,27 @@ AFRAME.registerSystem('game', {
       this.controller = new Controller();
 
       // Example summon a custom entity
-      this.entities = [
-        new ShapedItem(0, 2, -5, 'HEART'),
-        new ShapedItem(-10, 2, -5, 'HEART'),
-        new ShapedItem(10, 2, -5, 'CLOUD'),
-        new ShapedItem(7, 2, -5, 'LIGHTNING'),
-        new Room(),
-      ]
+      // this.entities = [
+      //   new ShapedItem(0, 2, -5, 'HEART'),
+      //   new ShapedItem(-10, 2, -5, 'HEART'),
+      //   new ShapedItem(10, 2, -5, 'CLOUD'),
+      //   new ShapedItem(7, 2, -5, 'LIGHTNING'),
+      //   new Room(),
+      // ]
+      this.floor = new Room();
+      this.startGame();
     },
 
     tick(time, timeDelta) {
       // Update game objects
       this.entities.forEach(entity => entity.update(time, timeDelta));
-    }
+    },
+
+    startGame() {
+      const { entities =[] } = this;
+      for (let i=0; i < 10; i++) {
+        entities.push(new ShapedItem());
+      }
+      this.entities = entities;
+    },
 });
