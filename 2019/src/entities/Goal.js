0d4880a575370ace85037fd76ede2b0e1c633e2e
Chris Richards <ripter001@gmail.com>
2019-08-17 21:42:07 -0700

    Cleaning up a little and starting to prep for Goal

---
+++ 2019/src/entities/Goal.js
@@ -0,0 +1,6 @@
+
+export class Goal {
+  constructor() {
+
+  }
+}
--- 2019/src/systems/game.js
+++ 2019/src/systems/game.js
@@ -1,6 +1,7 @@
 import { Controller } from '../entities/controller.js';
 import { Box } from '../entities/Box';
 import { Room } from '../entities/room.js';
+import { Goal } from '../entities/Goal.js'
 import { ShapedItem } from '../entities/ShapedItem.js';
 
 import { SHAPES } from '../shapes.js';
@@ -22,8 +23,6 @@ AFRAME.registerSystem('game', {
         entity: null,
       };
 
-      // this.cloudGeometery = new THREE.SphereGeometry( 5, 32, 32 );
-
       this.floor = new Room();
       this.startGame();
     },
@@ -34,63 +33,24 @@ AFRAME.registerSystem('game', {
     },
 
     setSelected(entity) {
-      if (entity === this.selected.entity) { return; }
-      console.log('setSelected', entity);
-
-      if (this.selected.entity) {
-        this.clearSelected();
-      }
-
+      if (entity === this.selected) { return; }
       // Pull entity to the player's hand.
-      // this.selected.isPull = true;
-      this.selected.entity = entity;
-      // entity.object3D.getWorldDirection(this.selected.orbitPosition);
-      // this.selected.orbitPosition.copy(entity.object3D.position);
-      // this.selected.orbitAttribute = entity.getAttribute('obit');
+      this.selected = entity;
 
-      entity.addEventListener('float-at-target', (event) => {
-        console.log('game responding to float-at-target', event);
-      });
+      // entity.addEventListener('float-at-target', (event) => {
+      //   console.log('game responding to float-at-target', event);
+      // });
       entity.addEventListener('float-completed', (event) => {
-        console.log('game responding to float-completed', event);
         entity.setAttribute('orbit', {active: true});
       });
 
-      // window.selectedEntity = entity;
-      // entity.removeAttribute('orbit');
       entity.setAttribute('orbit', {active: false});
       entity.setAttribute('float-to', {
-        // target: '#leftHand',
         targetPosition: this.elLeftHand.getAttribute('position'),
-        // targetPosition: this.elLeftHand.object3D.position,
         speed: 0.1,
       });
     },
 
-    clearSelected() {
-      console.log('clearSelected', this.selected.entity);
-      // this.selected.entity.setAttribute('float-to', {
-      //   targetPosition: this.selected.orbitPosition,
-      //   speed: 0.05,
-      // });
-      this.selected.entity = null;
-      return;
-      // const { entity, orbitPosition } = this.selected;
-      // // Push back into place.
-      // this.selected.isPull = false;
-      // entity.setAttribute('float-to', {
-      //   position: orbitPosition,
-      //   speed: 2,
-      // });
-      // this.selected.entity = null;
-    },
-
-    // getRandomCloudPoint() {
-    //   const { vertices } = this.cloudGeometery;
-    //   // limit to the upper hemisphere
-    //   const points = vertices.filter(v => v.y >= 2);
-    //   return points[THREE.Math.randInt(0, points.length)];
-    // },
 
     startGame() {
       const { entities =[] } = this;
