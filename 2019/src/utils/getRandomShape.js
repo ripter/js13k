6253ffd2de05a63ee69930630cd35e597ffac26e
Chris Richards <ripter001@gmail.com>
2019-10-12 17:03:44 -0700

    blank starter

--- 2019/src/utils/getRandomShape.js
+++
@@ -1,6 +0,0 @@
-import { SHAPES } from '../consts/shapes.js';
-const SHAPE_LIST = Object.keys(SHAPES);
-
-export const getRandomShape = (shapeList = SHAPE_LIST) => {
-  return shapeList[THREE.Math.randInt(0, shapeList.length-1)];
-};
