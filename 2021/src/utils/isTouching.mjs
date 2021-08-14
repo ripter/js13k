18644c5457380c7996a1eeeb35f4a7fab11fe7f9
Chris Richards <ripter001@gmail.com>
2021-08-14 01:03:32 -0700

    Player can "pick up" a sprite group by touching it.

--- 2021/src/utils/isTouching.mjs
+++ 2021/src/utils/isTouching.mjs
@@ -4,5 +4,7 @@
  * @returns True if the two entities are touching.
 */
 export function isTouching(entity1, entity2) {
-  return false;
+
+  return ((entity1.x+4) >= entity2.x && (entity1.x-4) <= (entity2.x+8))
+    && ((entity1.y+4) >= entity2.y && (entity1.y-4) <= (entity2.y+8));
 }
