6e52def33bce5d34d9b852dbf3cbc75f3b1e955a
Chris Richards <ripter001@gmail.com>
2021-08-26 22:46:39 -0700

    Can detect the player pressing the compact button.

--- 2021/src/systems/pushButtonSystem.mjs
+++ 2021/src/systems/pushButtonSystem.mjs
@@ -1,5 +1,28 @@
+import { byComponents } from '../entities/byComponents.mjs';
+import { byID } from '../entities/byID.mjs';
+import { getKey } from '../utils/key.mjs';
 
 
 export function pushButtonSystem(delta) {
+  const { downKeys } = byID('input');
+  const player = byID('player');
+  const pushButtonEntities = byComponents(['push-button']);
+  const playerKey = getKey(player);
 
+  // If the player isn't pressing up, we can skip the system.
+  // The button is hardcoded to be on the bottom of the compactor,
+  // if this changes, we will need a prop to know the button direction.
+  if (!downKeys.has('up')) {
+    return;
+  }
+
+  // Check if the player is in the right position to push the button.
+  for (let pushButton of pushButtonEntities) {
+    // Get get for the direction the player should be in to push it.
+    const buttonKey = getKey(pushButton, 0, 1);
+    if (playerKey === buttonKey) {
+      // Button Pressed!
+      console.log('Button Pressed!');
+    }
+  }
 }
