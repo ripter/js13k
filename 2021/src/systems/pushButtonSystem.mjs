05b3373f938e523d39443997742ee0592f4aaf32
Chris Richards <ripter001@gmail.com>
2021-08-27 22:47:20 -0700

    Moved mapValues to it's own file.

--- 2021/src/systems/pushButtonSystem.mjs
+++ 2021/src/systems/pushButtonSystem.mjs
@@ -6,17 +6,18 @@ import { pushAnimation } from '../animations/pushButton.mjs';
 
 export function pushButtonSystem(delta) {
   const { downKeys } = byID('input');
-  const player = byID('player');
+  const playerEntities = byComponents(['player']);
   const pushButtonEntities = byComponents(['push-button']);
-  const playerKey = getKey(player);
 
   // If the player isn't pressing up, we can skip the system.
   // The button is hardcoded to be on the bottom of the compactor,
   // if this changes, we will need a prop to know the button direction.
-  if (!downKeys.has('up')) {
+  if (playerEntities.size === 0 || !downKeys.has('up')) {
     return;
   }
 
+  const player = Array.from(playerEntities)[0];
+  const playerKey = getKey(player);
   // Check if the player is in the right position to push the button.
   for (let pushButton of pushButtonEntities) {
     // Get get for the direction the player should be in to push it.
@@ -30,5 +31,10 @@ export function pushButtonSystem(delta) {
     // Remove us from push-button so we don't show up in this reducer until the animation is over.
     pushButton.components.delete('push-button');
 
+
+
+    // Remove the player so they can not do anything until the animation is over.
+    // player.components.delete('player');
+    // player.components.add('player-disabled');
   }
 }
