3f848f9b0a9c7c492be3da781f55797695a32402
Chris Richards <ripter001@gmail.com>
2021-08-28 21:56:51 -0700

    seems to work

--- 2021/src/animations/crushWall.mjs
+++ 2021/src/animations/crushWall.mjs
@@ -2,84 +2,8 @@ import { genFrameAnimation } from './genFrameAnimation.mjs';
 
 export function* crushWallAnimation() {
   const generator = genFrameAnimation(18, 0.25, (props) => {
+    console.log('callback', props);
     const { entity, frame } = props;
-      switch (frame) {
-        case 0:
-          entity.components.add('sprite');
-          entity.color = 'dark_gray';
-          break;
-        case 1:
-          entity.color = 'light_gray';
-          break;
-        case 2:
-          entity.color = 'green';
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-          entity.x -= 8;
-          break;
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-          entity.x += 8;
-          break;
-          entity.color = 'light_gray';
-          break;
-        case 16:
-          entity.color = 'dark_gray';
-          break;
-        case 17:
-          entity.components.delete('sprite');
-          break;
-
-        default:
-          // do nothing.
-      }
-  // first call is init.
-  var { value, done } = generator.next();
-  while (!done) {
-    var { value, done } = generator.next();
-    console.log('value', value, 'done', done);
-    // yield* generator;
-    const props = yield;
-    console.log('props', props);
-  }
-
-
-  // const frameAnimation = genFrameAnimation(18, 0.25, (frame) => {
-  //   console.log('genFrameAnimation callback frame', frame);
-  // });
-  // console.log('frameAnimation', frameAnimation);
-
-  // for (let frameItem of genFrameAnimation(18, 0.25)) {
-  //   console.log('frameItem', frameItem);
-  // }
-
-  return;
-
-  const DURATION = 18;
-  const FRAME_DELAY = 0.25;
-  let frame = 0;
-  let delay = 0;
-  let props;
-
-  while (frame < DURATION) {
-    // YIELD
-    props = yield;
-    const { entity, deltaTime } = props;
-    // wait until the delay is over before doing the next animation.
-    if (delay > 0) {
-      delay -= deltaTime;
-      continue;
-    }
-
     switch (frame) {
       case 0:
         entity.components.add('sprite');
@@ -106,7 +30,6 @@ export function* crushWallAnimation() {
       case 14:
       case 15:
         entity.x += 8;
-        break;
         entity.color = 'light_gray';
         break;
       case 16:
@@ -115,14 +38,25 @@ export function* crushWallAnimation() {
       case 17:
         entity.components.delete('sprite');
         break;
-
       default:
-        // do nothing.
+      // do nothing.
     }
+  });
+  // first call is init.
+  var { value, done } = generator.next();
+
+  console.log('crusWall value', value, 'done', done);
 
 
-    frame += 1;
-    // Update the delay between animation frames.
-    delay = FRAME_DELAY;
+  // Run the Generator
+  // yield* generator;
+  let props = yield;
+  while (!done) {
+    var { value, done } = generator.next(props);
+    console.log('while(!done)', 'value', value, 'done', done);
+    // yield* generator;
+    props = yield;
+    console.log('props', props);
   }
+
 }
