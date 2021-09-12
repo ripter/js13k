2571e94563182d5b99e7bba953326dc264cd3958
Chris Richards <ripter001@gmail.com>
2021-09-11 21:55:14 -0700

    Updated piano to allows notes of different lengths.

--- 2021/src/consts/music.mjs
+++ 2021/src/consts/music.mjs
@@ -1,20 +1,30 @@
 import { playPiano } from '../utils/alphabetPiano.mjs';
 
+export const TEMPO = 120;
+
+export const NOTE_DURATION = {
+  W: 4, // whole note
+  H: 2, // half note
+  Q: 1, // quater note
+  E: 0.5,
+  S: 0.25,
+};
+
 export const MUSIC = [
     // 0 is idle song.
     () => {
-      playPiano('a<a>a@@b<b>a<a>a@@b<b>a<a>a@@b<b>', 0.3, 'triangle');
-      playPiano('a<a>a@@b<b>a<a>a@@b<b>a<a>a@@b<b>', 0.3, 'sine');
+      // playPiano('a<a>a@@b<b>a<a>a@@b<b>a<a>a@@b<b>', 0.3, 'triangle');
+      // playPiano('a<a>a@@b<b>a<a>a@@b<b>a<a>a@@b<b>', 0.3, 'sine');
     },
-    // 1 is a jazzy tune.
+    // 1 is a intro music.
     () => {
-      playPiano('Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0', 0.15, 'sine')
-      playPiano('hihi0why00hihi0why0querty0querty0hi0hi')
+      playPiano('aWaHaHaQaQaQaQ', 'sine');
+      // playPiano('aWbWaHbHaQbQ', 'sine');
+      // playPiano('Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0Z0ZaaZ0Z0W0W0', 0.15, 'sine')
+      // playPiano('hihi0why00hihi0why0querty0querty0hi0hi')
     },
     // 1 is main compactor
-    ['ZY0KH0YX0KH0WX', 0.3, 'sine'],
-    [
-      '@@@0AAA0BBB0CCC',
-      0.3, 'triangle'
-    ],
+    () => {
+
+    },
 ];
--- 2021/src/utils/alphabetPiano.mjs
+++ 2021/src/utils/alphabetPiano.mjs
@@ -1,29 +1,78 @@
+import { NOTE_DURATION, TEMPO } from '../consts/music.mjs';
+
 /**
  * Plays a string a-z with a piano sound.
- * copied from: https://xem.github.io/alphabet-piano/
- * @param  {string} melody - string of a-z chars of notes to play.
- * @param  {number} [noteLength=0.3] - length of each note.
+ * Inspired & heavily borrowed from: https://xem.github.io/alphabet-piano/
+ * @param  {string} melody - string of charCode/Duration pairs.
+ * @example - "aWbH" Duration W, H, Q, E, S
  * @param  {String} [waveform='sine'] - sine, triangle, square, or sawtooth.
  */
-export function playPiano(melody, noteLength=0.3, waveform='sine') {
+export function playPiano(melody, waveform='sine') {
   const actx = new AudioContext();
-  const G = actx.createGain();
-  G.connect(actx.destination);
+  // const gain = actx.createGain();
 
-  for(let i=0; i < melody.length; i++) {
+  let runningTime = 0;
+  for(let i=0; i < melody.length; i += 2) {
+    if (melody[i] === '0') { continue; }
+    const freq = 440*1.06**(-105+melody.charCodeAt(i));
+    const noteLength = 60 / TEMPO * NOTE_DURATION[melody[i+1]];
+    const startTime = runningTime+.3;
+    const endTime = startTime+noteLength-.01;
+    runningTime = endTime;
+
+    console.group('note');
+    console.log('i', i, 'noteLength', noteLength, 'melody', melody[i], 'freq', freq);
+    console.log('noteLength', melody[i+1], NOTE_DURATION[melody[i+1]], noteLength);
+    console.log('currentTime', actx.currentTime);
+    console.log('runningTime', runningTime);
+    console.log('startTime', startTime, actx.currentTime + startTime);
+    console.log('endTime', endTime, actx.currentTime + endTime);
+
+    const oscillator = new OscillatorNode(actx, {
+      type: waveform,
+      frequency: freq,
+    });
+
+    // oscillator.connect(actx.destination);
+
+
+
+    const gain = new GainNode(actx, {
+      gain: 0.5,
+    });
+    gain.gain.setTargetAtTime(0.001, endTime, 0.05);
+
+
+    oscillator.connect(gain);
+    gain.connect(actx.destination);
+
+
+    oscillator.start(startTime);
+    oscillator.stop(endTime);
+
+    console.groupEnd();
+
+    /*
     const note = actx.createOscillator();
-    if (melody[i=+i] && melody[i] != "0") {
-      const startTime = i*noteLength+.3;
-      note.connect(G);
       note.start(startTime);
-      note.frequency.setValueAtTime(440*1.06**(-105+melody.charCodeAt(i)),startTime);
-      note.type = waveform;
+      // note.frequency.setValueAtTime(freq,startTime);
+      // note.type = waveform;
       G.gain.setValueAtTime(.5,startTime),
       G.gain.setTargetAtTime(.001,startTime+.1,.05),
       note.stop(startTime+noteLength-.01);
-    }
+      // connect last, because? MDN says so.
+      note.connect(G);
+    */
+
   }
+
+  // These pages say to connect to the destination on the last step.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
+  // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Basic_concepts_behind_Web_Audio_API
+  // G.connect(actx.destination);
 }
+
+
 
 // For Easy create/debuggin
 window.playPiano = playPiano;
