6fd9e2ac89421f58c3448f3532e94c9d31cea0f1
Chris Richards <ripter001@gmail.com>
2021-10-02 20:25:58 -0700

    Adding Terser to minimize the code. Added visualizer and analyze to the build pipeline.

--- 2021/rollup.config.js
+++ 2021/rollup.config.js
@@ -1,6 +1,11 @@
+import { terser } from 'rollup-plugin-terser';
+import { visualizer } from 'rollup-plugin-visualizer';
+import analyze from 'rollup-plugin-analyzer';
 
-// Without any plugins, rollup combines all of the used source files into a single file.
+
 //
+// Without any plugins, rollup combines all of the used source files into a single file.
+// This converts the source inta 45 KB file dist/main.es.js (11 KB zipped)
 export default {
 	input: 'src/main.mjs',
 	output: {
@@ -11,6 +16,20 @@ export default {
     },
 	},
 	plugins: [
+		analyze({
+			summaryOnly: true,
+		}),
+		// default terser plugin.
+		// 17 KB main.es.js (5 KB zipped)
+		terser({
+			compress: {
+				drop_console: true,
+			},
+		}),
+		visualizer({
+			gzipSize: true,
+			template: 'sunburst', // 'sunburst', 'network', or  'treemap'
+		}),
 	],
 	external: [  ]
 };
