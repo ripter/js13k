f45bd5439bef80e4ed83c82422686e1317dee97e
Chris <ripter001@gmail.com>
2023-08-30 13:05:26 -0400

    style updates

--- 2023/src/elements/scenario-picker.mjs
+++ 2023/src/elements/scenario-picker.mjs
@@ -7,64 +7,64 @@ class ScenarioPicker extends HTMLElement {
     }
 
     connectedCallback() {
-      this.render();
-      this.addEventListener('click', this.handleClick);
+        this.render();
+        this.addEventListener('click', this.handleClick);
     }
+
     disconnectedCallback() {
-      this.removeEventListener('click', this.handleClick);
+        this.removeEventListener('click', this.handleClick);
     }
 
     get options() {
         return this._options;
     }
+
     set options(val) {
         this._options = val;
         this.render();
     }
+
     get selectedOption() {
-      const elmOption = this.querySelector(`.option.${CLASS_EXPANDED}`)
-      if (!elmOption) { return null }
-      const idx = parseFloat(elmOption.dataset.idx);
-      return this.options[idx];
+        const elmOption = this.querySelector(`.option.${CLASS_EXPANDED}`)
+        if (!elmOption) return null;
+        const idx = Number(elmOption.dataset.idx);
+        return this.options[idx];
     }
 
-  async render() {
-    // Generate options as a string
-    const optionsHTML = this._options.map((option, idx) =>
-      `<div class="option" data-idx="${idx}">
-        <dt>${option.name}</dt>
-        <dd>
-          ${option.description}
-          <button>🚀 Embark</button>
-        </dd>
-        </div>`
-    ).join('');
+    async render() {
+        const optionsHTML = this._options.map((option, idx) =>
+            `<div class="option" data-idx="${idx}">
+                <dt>${option.name}</dt>
+                <dd>
+                    ${option.description}
+                    <button>🚀 Embark</button>
+                </dd>
+            </div>`
+        ).join('');
 
-    // Embed the structure using a template literal
-    this.innerHTML = `<dl>
-      ${optionsHTML}
-    </dl>`;
-  }
-
-  handleClick(evt) {
-    const { target } = evt;
-    const elmOption = target.closest('.option');
-    const shouldToggleExpand = target.tagName === 'DT';
-    const isButton = target.tagName === 'BUTTON';
-    const isExpanded = elmOption.classList.contains(CLASS_EXPANDED);
-
-    if (isButton) {
-      this.dispatchEvent(new CustomEvent('option-confirmed', { detail: this.selectedOption }));
+        this.innerHTML = `<dl>
+            ${optionsHTML}
+        </dl>`;
     }
-    else if (shouldToggleExpand) {
-      // clear out any previously expanded items.
-      this.querySelectorAll('.option.expanded').forEach(elm => elm.classList.remove(CLASS_EXPANDED))
-      // Add the Expanded class to the current option.
-      if (!isExpanded) {
-        elmOption.classList.add(CLASS_EXPANDED);
-      }
-    } 
-  }
+
+    handleClick(evt) {
+        const { target } = evt;
+        const elmOption = target.closest('.option');
+        if (!elmOption) return; // Ensure all actions are related to an option.
+
+        const shouldToggleExpand = target.tagName === 'DT';
+        const isButton = target.tagName === 'BUTTON';
+        const isExpanded = elmOption.classList.contains(CLASS_EXPANDED);
+
+        if (isButton) {
+            this.dispatchEvent(new CustomEvent('option-confirmed', { detail: this.selectedOption }));
+        } else if (shouldToggleExpand) {
+            this.querySelectorAll('.option.expanded').forEach(elm => elm.classList.remove(CLASS_EXPANDED))
+            if (!isExpanded) {
+                elmOption.classList.add(CLASS_EXPANDED);
+            }
+        }
+    }
 }
 
 customElements.define('scenario-picker', ScenarioPicker);
