7c9ac32ffdb4e60472e41c3d98add8cddd4b7aa0
Chris <ripter001@gmail.com>
2023-09-06 20:54:03 -0400

    WIP player hand

--- 2023/src/elements/player-hand.mjs
+++ 2023/src/elements/player-hand.mjs
@@ -2,11 +2,6 @@
 class PlayerHand extends HTMLElement {
   render(props) {
     const cards = JSON.parse(this.getAttribute('cards') ?? '[]');
-    // const html = `<div class="hand-container">
-    //   ${cards.map(cardKey => `
-    //     <card-challenge class="card-in-hand" data-card-idx="${cardKey}"></card-challenge>
-    //   `).join('')}
-    // </div>`;
     const html = `${cards.map(cardKey => `<div class="flex-column">
       <card-challenge class="card-in-hand" data-card-idx="${cardKey}"></card-challenge>
       <button class="w-5">Pick</button>
